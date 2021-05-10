bac864ec5512557b2e9f8a11be3e55030ad0f267
kylnobar <kylnobar@gmail.com>
2021-05-09 20:39:06 -0700

    Add project files.

---
+++ BinaryClock.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Binary_Clock
+{
+    public class BinaryClock : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D BackgroundTexture;
+        private Texture2D pipTexture;
+        private XYMapper Pipper;
+
+        private int HourLeftPos = 46;
+        private int HourRightPos = 96;
+        private int MinuteLeftPos = 170;
+        private int MinuteRightPos = 220;
+        private int SecondLeftPos = 296;
+        private int SecondRightPos = 346;
+
+        public BinaryClock()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 416;
+            _graphics.PreferredBackBufferHeight = 312;
+            _graphics.ApplyChanges();
+
+            Pipper = new XYMapper();
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            BackgroundTexture = Content.Load<Texture2D>("Sprites/background");
+            pipTexture = Content.Load<Texture2D>("Sprites/pip");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(BackgroundTexture, new Vector2(0,0), Color.White);
+
+            Pipper.DrawPips(Pipper.getRightDigit(DateTime.Now.Second), SecondRightPos, _spriteBatch, pipTexture);
+            Pipper.DrawPips(Pipper.getLeftDigit(DateTime.Now.Second), SecondLeftPos, _spriteBatch, pipTexture);
+
+            Pipper.DrawPips(Pipper.getRightDigit(DateTime.Now.Minute), MinuteRightPos, _spriteBatch, pipTexture);
+            Pipper.DrawPips(Pipper.getLeftDigit(DateTime.Now.Minute), MinuteLeftPos, _spriteBatch, pipTexture);
+
+            Pipper.DrawPips(Pipper.getRightDigit(DateTime.Now.Hour), HourRightPos, _spriteBatch, pipTexture);
+            Pipper.DrawPips(Pipper.getLeftDigit(DateTime.Now.Hour), HourLeftPos, _spriteBatch, pipTexture);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ XYMapper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Binary_Clock
+{
+    class XYMapper
+    {
+        private int bit1Pos = 212;
+        private int bit2Pos = 148;
+        private int bit4Pos = 84;
+        private int bit8Pos = 20;
+        public void DrawPips(int digit, int xCoordinate, SpriteBatch spriteBatch, Texture2D pipTexture)
+        {
+            //digit = 11;  //Uncomment to turn all pips on
+
+            switch (digit)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+
+                case 2:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit2Pos), Color.White);
+                    break;
+
+                case 3:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit2Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+
+                case 4:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit4Pos), Color.White);
+                    break;
+
+                case 5:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit4Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+
+                case 6:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit4Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit2Pos), Color.White);
+                    break;
+
+                case 7:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit4Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit2Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+
+                case 8:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit8Pos), Color.White);
+                    break;
+
+                case 9:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit8Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+
+                default:
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit8Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit4Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit2Pos), Color.White);
+                    spriteBatch.Draw(pipTexture, new Vector2(xCoordinate, bit1Pos), Color.White);
+                    break;
+            }
+
+        }
+
+        public int getLeftDigit(int number)
+        {
+            return number / 10;
+        }
+
+        public int getRightDigit(int number)
+        {
+            return number % 10;
+        }
+    }
+}
